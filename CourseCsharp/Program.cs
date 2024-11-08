7eb991b361ebe2a08ead2e72ea32a440dfc55b0a
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-11-08 18:41:57 -0300

    Adicionar arquivos de projeto.

---
+++ CourseCsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+namespace CourseCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste");
+
+            Console.WriteLine("Quanto numero inteiros voce vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Oi");
+            }
+        }
+    }
+}
